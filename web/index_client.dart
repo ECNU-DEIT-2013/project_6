108b7bce8380c1027e24d7ec4dcca8b1601e5893
azhangyilei <azhangyilei@163.com>
2015-11-17 19:08:22 +0800

    project_6
    搭建共有界面

--- web/index_client.dart
+++ web/index_client.dart
@@ -1,20 +1,24 @@
 import 'dart:html';
 import'dart:convert' ;
-import 'package:sqljocky/sqljocky.dart';
 
-var username;
-var password;
-var pool;
-var results;
-InputElement text;
-List jsondata=[];
+InputElement username;
+InputElement password;
+List list;
 
 void main() {
-  querySelector('#test').text = "hello";
-  username = document.getElementById('#user_name');
-  password =  document.getElementById('#user_password');
-  text=querySelector('user_name');
+  username =querySelector('#user_name');
+  password = querySelector('#user_password');
+  //list.add(username.text);
+ // list.add(password.text);
+  //querySelector('#register').onClick.listen(user_register);
+  querySelector('#test_button').onClick.listen(user_register);
 
 }
 
-
+void user_register(Event e){
+  var path = 'http://127.0.0.1:8080';
+  var httprequest=new HttpRequest();
+    httprequest
+      ..open('POST',path)
+      ..send(JSON.encode(username.value));
+}
