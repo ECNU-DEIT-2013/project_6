bfe9bc43a43ccc23976b686d6524a81a45c7bbde
YJZHOU <yjzhouecnu@163.com>
2015-11-26 14:05:12 +0800

    stuform.dart错误测试

--- bin/index_server.dart
+++ bin/index_server.dart
@@ -10,6 +10,7 @@ main() async {
     addCorsHeaders(request.response);
     var jsondata = await request.transform(UTF8.decoder).join();
     await register(jsondata);
+    //await save(jsondata);
     request.response.close();
   }
 }
@@ -28,4 +29,21 @@ register(var x) async{
   var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340202');
   var query = await pool.prepare('insert into user_zl (password, name) values (?, ?)');
   await query.execute(['${password}', '${name}']);
+}
+
+save(var x) async{
+  var s =JSON.decode(x);
+  var sex=s[0];
+  var department=s[1];
+  var major=s[2];
+  var grade=s[3];
+  var dorm=s[4];
+  var email=s[5];
+  var tel=s[6];
+  print(s);
+  print(sex);
+  print(department);
+  //var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340210');
+ // var query = await pool.prepare('insert into user_inf (user_sex,user_department,user_major,user_grade,user_dorm,user_email,user_tel) values (?, ?, ?, ?, ?, ?, ?)');
+ // await query.execute(['${sex}', '${department}']);
 }
