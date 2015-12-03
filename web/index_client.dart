d097cac3a69ba53422c07eb7fdf345f17f875445
azhangyilei <azhangyilei@163.com>
2015-12-03 14:32:16 +0800

    添加密码验证

--- web/index_client.dart
+++ web/index_client.dart
@@ -3,6 +3,7 @@ import 'dart:html';
 import'dart:convert' ;
 import 'package:cookie/cookie.dart'as cookie;
 
+
 var username;
 var password;
 List list=[];
@@ -18,11 +19,14 @@ void user_register(Event e) {
   password = document.getElementById('user_password').value;//获取密码
   if(username.length==0) {
     window.alert('用户名为空');
+
   }
   else if(password.length==0){;
   window.alert('密码为空');
+
   }
   else{
+    window.location.href="stu.html";
     list.add(username);
     list.add(password);//在list上加入用户名和密码的信息
     var path ='http://127.0.0.1:8080/index';
