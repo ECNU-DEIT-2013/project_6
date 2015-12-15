2aff6c84430bc5886ae5c30000711f2481efdc91
azhangyilei <azhangyilei@163.com>
2015-12-15 09:13:31 +0800

    conflict1

--- web/studentpage.dart
+++ web/studentpage.dart
@@ -1,8 +1,17 @@
 import 'dart:html';
 import 'package:cookie/cookie.dart'as cookie;
+
+ImageElement button_3;
+ImageElement button_2;
+InputElement todoInput;
 void main(){
-  var s=document.getElementById('test');
-  var string=cookie.get('email');
-  s.value=string;
-  print("qs");
+ // var name=cookie.get('name');
+
+ //document.getElementById('button_3').src="index_picture/button2.png";
+  todoInput=querySelector('#test');
+  todoInput.value="hello";
+button_3=querySelector('#button_3');
+  button_2=querySelector('#button_2');
+button_3.src="index_picture/button3.png";
+  button_2.src="index_picture/button2.png";
 }
