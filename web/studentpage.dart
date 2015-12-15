b1856cf013a43b89d39e333dfc7f830a5f3756cf
azhangyilei <azhangyilei@163.com>
2015-12-15 12:31:36 +0800

    change

--- web/studentpage.dart
+++ web/studentpage.dart
@@ -6,12 +6,11 @@ ImageElement button_2;
 InputElement todoInput;
 void main(){
  // var name=cookie.get('name');
-
  //document.getElementById('button_3').src="index_picture/button2.png";
   todoInput=querySelector('#test');
   todoInput.value="hello";
-button_3=querySelector('#button_3');
+  button_3=querySelector('#button_3');
   button_2=querySelector('#button_2');
-button_3.src="index_picture/button3.png";
+  button_3.src="index_picture/button3.png";
   button_2.src="index_picture/button2.png";
 }
