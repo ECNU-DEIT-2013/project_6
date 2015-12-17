1b7982df3ae29e7757f169348e794835b8f2c5a0
hukuyamajun <645271296@qq.com>
2015-12-17 20:51:38 +0800

    新建社团信息页面
    test

--- web/studentpage.dart
+++ web/studentpage.dart
@@ -2,20 +2,6 @@ import 'dart:html';
 import 'package:cookie/cookie.dart'as cookie;
 import 'dart:convert';
 
-<<<<<<< HEAD
-ImageElement button_3;//å­˜æ”¾å›¾ç‰‡
-InputElement todoInput;
-List<String> my_email=[];//å­˜æ”¾ä»æœåŠ¡å™¨ä¸­ä¼ è¿‡æ¥çš„å€¼
-void main(){
-  var name=cookie.get('name');
-  document.getElementById('test').value=name.toString();
-  var path = 'http://127.0.0.1:8080/studentpage';//å”¯ä¸€çš„åœ°å€URL
-  var httpRequest = new HttpRequest();//è®¿é—®webç½‘ç«™çš„åè®®ï¼ˆç½‘ä¸Šè¯·æ±‚æ–‡ä»¶â€”â€”ftpè¶…æ–‡æœ¬ä¼ è¾“åè®®ï¼‰
-  httpRequest
-    //..open('GET', path)//æ‹¨å·ï¼Œæ‰“å¼€ä¸€æ¡é€šé“
-    ..open('POST', path)
-    ..send(JSON.encode(name))//æŠŠç”¨æˆ·åä¼ ç»™æœåŠ¡å™¨ï¼ŒæœåŠ¡å™¨æ ¹æ®ç”¨æˆ·åè¿›è¡ŒæŸ¥æ‰¾ä¸ç”¨æˆ·ååŒ¹é…çš„ä¿¡æ¯
-=======
 ImageElement button_3;//´æ·ÅÍ¼Æ¬
 InputElement todoInput;
 List<String> my_email=[];//´æ·Å´Ó·şÎñÆ÷ÖĞ´«¹ıÀ´µÄÖµ
@@ -28,27 +14,11 @@ void main(){
   //..open('GET', path)//²¦ºÅ£¬´ò¿ªÒ»ÌõÍ¨µÀ
     ..open('POST', path)
     ..send(JSON.encode(name))//°ÑÓÃ»§Ãû´«¸ø·şÎñÆ÷£¬·şÎñÆ÷¸ù¾İÓÃ»§Ãû½øĞĞ²éÕÒÓëÓÃ»§ÃûÆ¥ÅäµÄĞÅÏ¢
->>>>>>> refs/remotes/origin/10130340223
     ..onLoadEnd.listen((e) => requestComplete(httpRequest));
 }
 
 requestComplete(HttpRequest request) {
   if (request.status == 200) {
-<<<<<<< HEAD
-    //200ä»£è¡¨æ•°æ®æ­£ç¡®æ‹¿åˆ°
-    my_email = JSON.decode(request.responseText);
-    var num=my_email.length;
-    //document.getElementById('test').value=my_email.toString();//åˆ¤æ–­æ˜¯å¦æœ‰æ–°æ¶ˆæ¯
-    button_3=querySelector('#button_3');
-    if(num==0) {
-      button_3.src="index_picture/button3.png";//åœ¨æ²¡æœ‰æ–°æ¶ˆæ¯çš„æƒ…å†µä¸‹ï¼Œä½¿ç”¨button_3å›¾ç‰‡
-    }
-    else{
-      button_3.src="index_picture/button3_1.png";//æœ‰äº†æ–°æ¶ˆæ¯ä¹‹åä¼šä½¿ç”¨button_3_1å›¾ç‰‡
-    }
-  }
-}
-=======
     //200´ú±íÊı¾İÕıÈ·ÄÃµ½
     my_email = JSON.decode(request.responseText);
     var num=my_email.length;
@@ -62,4 +32,3 @@ requestComplete(HttpRequest request) {
     }
   }
 }
->>>>>>> refs/remotes/origin/10130340223
