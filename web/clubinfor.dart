d51b475ba4b064f1bc845cd7fd2a74d6c265dada
hukuyamajun <645271296@qq.com>
2015-12-18 14:33:37 +0800

    社团信息页面

--- web/clubinfor.dart
+++ web/clubinfor.dart
@@ -4,26 +4,20 @@ ImageElement picture1,picture2,picture3,picture4;
 var num=0;
 List<String> club_picture=[];
 void main(){
-<<<<<<< HEAD
- /*for(int i=1;i<5;i++)
- {club_picture[i]='club_picture/picture'+i.toString();
- }*/
-=======
- for(int i=1;i<9;i++)
- {club_picture[i]='club_picture/picture'+i.toString();
- }
->>>>>>> refs/remotes/origin/10130340223
-picture1=querySelector("#picture1");
-picture2=querySelector("#picture2");
-picture3=querySelector("#picture3");
-picture4=querySelector("#picture4");
-picture1.src="club_picture/picture1.jpg";
-picture2.src="club_picture/picture2.jpg";
-picture3.src="club_picture/picture3.jpg";
-picture4.src="club_picture/picture4.jpg";
+  for(int i=1;i<9;i++)
+  {club_picture[i]='club_picture/picture'+i.toString();
+  }
+  picture1=querySelector("#picture1");
+  picture2=querySelector("#picture2");
+  picture3=querySelector("#picture3");
+  picture4=querySelector("#picture4");
+  picture1.src="club_picture/picture1.jpg";
+  picture2.src="club_picture/picture2.jpg";
+  picture3.src="club_picture/picture3.jpg";
+  picture4.src="club_picture/picture4.jpg";
 
- querySelector("#last").onClick.listen(lbutton);
- querySelector("#next").onClick.listen(nbutton);
+  querySelector("#last").onClick.listen(lbutton);
+  querySelector("#next").onClick.listen(nbutton);
 }
 void lbutton(Event e){
   if(num==1) {alert('已经是最前页！');}
@@ -38,7 +32,7 @@ void lbutton(Event e){
 
 }
 void nbutton(Event e) {
-if(num!=5){
+  if(num!=5){
     num = num + 4;
     picture1.src="club_picture[num]";
     picture2.src="club_picture[num+1]";
