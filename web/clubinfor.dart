9d5d9a7d11fa42e0b4151242f9b00aed46a0637e
hukuyamajun <645271296@qq.com>
2015-12-21 18:27:29 +0800

    添加社团页面

--- web/clubinfor.dart
+++ web/clubinfor.dart
@@ -4,9 +4,7 @@ ImageElement picture1,picture2,picture3,picture4;
 var num=0;
 List<String> club_picture=[];
 void main(){
-  for(int i=1;i<9;i++)
-  {club_picture[i]='club_picture/picture'+i.toString();
-  }
+
   picture1=querySelector("#picture1");
   picture2=querySelector("#picture2");
   picture3=querySelector("#picture3");
@@ -15,31 +13,36 @@ void main(){
   picture2.src="club_picture/picture2.jpg";
   picture3.src="club_picture/picture3.jpg";
   picture4.src="club_picture/picture4.jpg";
+  for(int i=1;i<9;i++){
+  club_picture.add('club_picture/picture'+i.toString()+'.jpg');
+  //document.getElementById('test').value=club_picture[0];
+  }
+
 
   querySelector("#last").onClick.listen(lbutton);
   querySelector("#next").onClick.listen(nbutton);
 }
 void lbutton(Event e){
-  if(num==1) {alert('ÒÑ¾­ÊÇ×îÇ°Ò³£¡');}
+  if(num==0) {alert('å·²ç»æ˜¯æœ€å‰é¡µï¼');}
   else{
     num=num-4;
-    picture1.src="club_picture[num]";
-    picture2.src="club_picture[num+1]";
-    picture3.src="club_picture[num+2]";
-    picture4.src="club_picture[num+3]";
+    picture1.src=club_picture[num];
+    picture2.src=club_picture[num+1];
+    picture3.src=club_picture[num+2];
+    picture4.src=club_picture[num+3];
   }
 
 
 }
 void nbutton(Event e) {
-  if(num!=5){
+  if(num!=4){
     num = num + 4;
-    picture1.src="club_picture[num]";
-    picture2.src="club_picture[num+1]";
-    picture3.src="club_picture[num+2]";
-    picture4.src="club_picture[num+3]";
+    picture1.src=club_picture[num];
+    picture2.src=club_picture[num+1];
+    picture3.src=club_picture[num+2];
+    picture4.src=club_picture[num+3];
   }
   else {
-    alert('ÒÑ¾­ÊÇ×îºóÒ»Ò³£¡');
+    alert('å·²ç»æ˜¯æœ€åŽä¸€é¡µ!');
   }
 }
