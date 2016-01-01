75a2c679e335999e73d6bf7435873ee7fff70668
Mingyue Cui <yueyueyiyiyou@hotmail.com>
2016-01-01 22:35:39 +0800

    終わり

--- bin/index_server.dart
+++ bin/index_server.dart
@@ -2,19 +2,25 @@ import 'dart:io';
 import 'package:sqljocky/sqljocky.dart';
 import 'dart:convert';
 import 'package:rest_frame/rest_frame.dart';
-//import 'package:cookie/cookie.dart'as cookie;
-//import 'dart:io';
+import 'package:mailer/mailer.dart';
 
-final DATA_FILE = "temp.json";
 Router routerindex = new Router();
 Router routerstuform = new Router();
 Router routercheck = new Router();
 var jsondata;
 //全局变量，用于接收客户端传来的数据。
 List my_email=[];
+List emailaddress=[];
+List my_stu=[];
+List club_send=[];
+List clubuser=[];
+List club_infor=[];
+var  name_check;
+var check_name;
+var register_check;
 main() async {
 
-var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8015);
+  var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8014);
   print("Serving at ${server.address}:${server.port}");
   await for (HttpRequest request in server) {
     addCorsHeaders(request.response);
@@ -23,9 +29,12 @@ var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8015);
     //register(jsondata);
     //save(jsondata);
     if (request.uri.path == "/index") {
+      await register();
       print("index page");
-      register();
-      routerindex.route(request);
+      await request.response
+        ..headers.contentType = new ContentType("application", "json", charset: "utf-8");
+      request.response.write(JSON.encode(name_check));
+      request.response.close();
     }
     else if (request.uri.path == "/stuform") {
       save();
@@ -33,22 +42,21 @@ var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8015);
       routerstuform.route(request);
     }
     else if (request.uri.path == "/check") {
-      check(request);
+      await check();
       print("check page");
-      print(request.method);
-
-
-      //handleGet(request);
-      routercheck.route(request);
+      await request.response
+        ..headers.contentType = new ContentType("application", "json", charset: "utf-8");
+      request.response.write(JSON.encode(check_name));
+      request.response.close();
     }
-    /*else if (request.uri.path == "/temp.json") {
-      print("read json");
-      handleGet(request);
-      routercheck.route(request);
+    else if(request.uri.path =='/stusign'){
+      //print("222222");
+      sign();
+      print("student_sign page");
+      routerstuform.route(request);
     }
-*/
     else if (request.uri.path == "/email") {
-      await studentpage();
+      await email();
       print("email page");
       await request.response
         ..headers.contentType = new ContentType("application", "json", charset: "utf-8");
@@ -56,13 +64,120 @@ var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8015);
         request.response.close();
       print("hello");
       print(my_email);
-      //my_email=[];
+      my_email=[];
      // routercheck.route(request);
+    }
+    else if (request.uri.path == "/studentpage") {
+      await studentpage();
+      print("studentpage page");
+      await request.response
+        ..headers.contentType = new ContentType("application", "json", charset: "utf-8");
+      request.response.write(JSON.encode(my_stu));
+      request.response.close();
+      print("hello");
+      print(my_email);
+    }
+    else if(request.uri.path == "/clubsend"){
+      await clubsend();
+      print("clubsend page");
+      await request.response
+        ..headers.contentType = new ContentType("application", "json", charset: "utf-8");
+      request.response.write(JSON.encode(club_send));
+      request.response.close();
+      print(club_send);
+      club_send=[];}
+
+
+    else if(request.uri.path == "/clubsendstu"){//根据客户端的查询要求，从数据库选择相应数据并传回客户端
+      if (jsondata!="") {
+        await clubsql();
+        await request.response
+          ..headers.contentType = new ContentType("application", "json", charset: "utf-8");
+        await request.response.write(JSON.encode(clubuser));
+        request.response.close();
+        print(clubuser);
+        clubuser=[];
+      }
+    }
+    else if(request.uri.path == "/clubsendsql"){//将发送的通知写入数据库
+      var sqllist = JSON.decode(jsondata);
+      var i;
+      print(sqllist);
+      var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340210');
+      for (i=0;i<sqllist.length;i++){
+        await pool.query(sqllist[i].toString());}
+
+
+    }
+    else if(request.uri.path == "/clubsendemail"){//获取需要发送邮件用户的邮箱地址
+      var check_name = JSON.decode(jsondata);
+      var i;
+
+      print(check_name);
+      var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340210');
+      for (i=0;i<check_name.length;i++){
+        var sql='select user_email from user_inf where user_name = "'+check_name[i]+'"';
+        var results = await pool.query(sql.toString());
+        await results.forEach((row) {
+          print('email: ${row[0]}');
+          emailaddress.add('${row[0]}');
+        });
+      }
+      print(emailaddress);
+
+    }
+    else if(request.uri.path == "/clubsendmessage"){//获取message，并发送邮件
+      var i;
+      var message =jsondata;
+      var options = new SmtpOptions()
+        ..hostName='smtp.student.ecnu.edu.cn'
+        ..port=25
+      // ..secured=true
+      // ..requiresAuthentication=true
+        ..username = '10130340210@student.ecnu.edu.cn'
+        ..password = 'mo376594393';
+      var emailTransport = new SmtpTransport(options);
+      for (i=0;i<emailaddress.length;i++){
+      var envelope = new Envelope()
+        ..from = '10130340210@student.ecnu.edu.cn'
+        ..recipients.add(emailaddress[i])
+        ..subject = 'ECNU_CLUB 社团通知'
+        ..text = message;
+      emailTransport.send(envelope);
+     // .then(
+     //         (envelope) => print('Email sent!'))
+     // .catchError(
+     //         (e) => print('Error occurred: $e'));
+      }
+      //发送邮件
+
+    }
+    else if(request.uri.path == "/clubinfor") {//从数据库调用社团信息到客户端
+      await clubinfor();
+      print("clubinfor page");
+      request.response
+        ..headers.contentType = new ContentType("application", "json", charset: "utf-8");
+      request.response.write(JSON.encode(club_infor));
+      request.response.close();
+      print("hello");
+      print(club_infor);
+
+    }
+    else if(request.uri.path == "/contact"){//将发送的建议写入数据库
+      var advice = JSON.decode(jsondata);
+      print(advice);
+      var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340210');
+      var query = await pool.prepare('insert into contact(advice) values (?)');
+     await query.execute(['${advice}']);
     }
     else {
       print("error!");
+      /**
+       * 这行本来在括号下面，逻辑有问题。导致你还没有发送有效的地址请求，服务器就到了最后直接关闭了response。
+       */
+      request.response.close();
     }
-    request.response.close();
+
   }
   routerindex.get(register, "/index");
   routerstuform.get(save, "/stuform");
@@ -76,16 +191,35 @@ void addCorsHeaders(HttpResponse res) {
 }
 
 register() async {
+  print("hello");
   var s = JSON.decode(jsondata);
   var name = s[0];
   var password = s[1];
   print(name);
   print(password);
   var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340210');
-  var query = await pool.prepare('insert into login (password, name) values (?, ?)');
-  await query.execute(['${password}', '${name}']);
-}
+  var string = 'select * from login where name = "${name}" ';
+  var results = await pool.query(string);
+  await results.forEach((row) {
+    print('name: ${row[0]}');
+    register_check='${row[0]}';
+  });
+  if(register_check==null){
+    var query = await pool.prepare('insert into login (password, name) values (?, ?)');
+    await query.execute(['${password}', '${name}']);
+    print("insert");
+    name_check="true";
+  }
+  else{
+    print("已注册");
+    name_check="";
+  }
+  print(name_check);
+  print(register_check);
+  register_check=null;
+  print(register_check);
 
+}
 save() async{
   var s = JSON.decode(jsondata);
   var name = s[0];
@@ -102,7 +236,7 @@ save() async{
   await query.execute(['${name}', '${sex}', '${department}', '${major}', '${grade}', '${dorm}', '${email}', '${tel}']);
 }
 
-check(HttpRequest request) async{
+check() async{
   var a;
   List list = [];
   var s = JSON.decode(jsondata);
@@ -113,7 +247,6 @@ check(HttpRequest request) async{
   var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340210');
   var string = 'select * from login where name = "${name}" and password = "${password}" ';
   var results = await pool.query(string);
-
   await results.forEach((row) {
     print('name: ${row[0]},password: ${row[1]}');
     a = '${row[1]}';
@@ -121,61 +254,102 @@ check(HttpRequest request) async{
 
   });
   //print(a);
-  if (a != null){print('ok');
-  File file=new File('temp.json');
-    file.writeAsString(JSON.encode('ok'));
- // Map<String,String> rs={'check':'ok'};
-  //request.session.addAll(rs);
+  if (a != null) {
+    print('ok');
+    //var file=new File('temp.json');
+    check_name="ok";
+    print(check_name);
   }
- // HttpSession.addAll('CHECK','OK');
-
-  else {
+   // r_check="ok";
+   // file.writeAsString(JSON.encode('ok'));}
+  else
+  {
     print('error');
-    File file=new File('temp.json');
-    file.writeAsString(JSON.encode('ERROR'));
-  }
+    check_name="error";
+    print(check_name);
+   // var file=new File('temp.json');
+  //  r_check="error";
+  //  file.writeAsString(JSON.encode('error'));
+  };
   // print(list);
 
 }
 
-studentpage() async{
-
+email() async{
+  var name = JSON.decode(jsondata);
+  print (name);
   print("begin connect");
   var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340210');
-  var results = await pool.query('select club_name,club_inf from club_inf');
+  var results = await pool.query('select club_name,user_name,message from user_message where user_name= "${name}" ');
   await results.forEach((row) {
-    print('club: ${row[0]},inf: ${row[1]}');
+    print('club: ${row[0]},user: ${row[2]}');
     my_email.add('${row[0]}');
-    my_email.add('${row[1]}');
+    my_email.add('${row[2]}');
   });
   print (my_email);
   print("connect");
- /** var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8015);
-  print("Serving at ${server.address}:${server.port}");
-  await for (var request in server) {
-    HttpResponse res = request.response;
-    addCorsHeaders(res);
-    res
-      ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
-      ..write(JSON.encode(jsondata))
-      ..close();
-  }*/
+}
+
+
+studentpage() async{
+  var name = JSON.decode(jsondata);
+  print("begin connect");
+  var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340210');
+  var results = await pool.query('select club_name,user_name,message from user_message where user_name= "${name}" ');
+  await results.forEach((row) {
+    print('club: ${row[0]},inf: ${row[2]}');
+    my_stu.add('${row[0]}');
+    my_stu.add('${row[2]}');
+  });
+  print (my_stu);
+  print("connect");
+}
+
+clubsend() async{
+
+  print("begin connect");
+  var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340210');
+  var results = await pool.query('select distinct club_name from club_user');
+  await results.forEach((row) {
+    print('clubname: ${row[0]}');
+    club_send.add('${row[0]}');
+  });
+  print (club_send);
 
 }
-/*void handleGet(HttpRequest req) {
-  HttpResponse res = req.response;
-  print("${req.method}: ${req.uri.path}");
-  addCorsHeaders(res);
+clubsql() async{
+  var sql = jsondata;
+  print(sql);
+  var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340210');
+  var results = await pool.query(sql);
+  await results.forEach((row) {
+    print('username: ${row[0]}');
+    clubuser.add('${row[0]}');
+  });
 
-  var file = new File(DATA_FILE);
-  if (file.existsSync()) {
-    res.headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
-    file.readAsBytes().asStream().pipe(res); // automatically close output stream
-  }
-  else {
-    var err = "Could not find file: $DATA_FILE";
-    //res.addString(err);
-    res.close();
-  }
-
-}*/
+}
+//连接数据库，调用社团信息
+clubinfor() async{
+  //print("begin ");
+  var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340210');
+  var results = await pool.query('select club_inf from club_inf ');
+  await results.forEach((row) {
+    //print('club_information: ${row[0]}');
+    club_infor.add('${row[0]}');
+  });
+//print (club_infor);
+//print("yes");
+}
+sign() async{
+  var s = JSON.decode(jsondata);
+  var stu_for = s[0];
+  var stu_hobby = s[1];
+  var stu_honor = s[2];
+  var stu_think = s[3];
+  var stu_fut = s[4];
+  var user_name=s[5];
+  print(s);
+  var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340210');
+  var query = await pool.prepare('insert into sign (clubname,hobby,honor,think,future,name) values (?, ?, ?, ?, ?, ?)');
+  await query.execute(['${stu_for}', '${stu_hobby}', '${stu_honor}', '${stu_think}', '${stu_fut}', '${user_name}']);
+}
---
+++ web/student_sign.dart
@@ -0,0 +1,101 @@
+import 'dart:html';
+import 'dart:convert';
+import 'package:cookie/cookie.dart'as cookie;
+import 'package:rest_frame/rest_frame.dart';
+import 'package:dialog/dialog.dart';
+
+InputElement  user_for;
+InputElement userhobby_in;
+InputElement userhonor_in;
+InputElement userthink_in;
+InputElement userfut_in;
+
+
+var userfor;
+var userhobby;
+var userhonor;
+var userthink;
+var userfut;
+
+List list=[];
+
+
+void main() {
+  var name=cookie.get('name');
+  var password =cookie.get('password');
+  //querySelector('#user_name').text = name.toString();
+  if(name==null){
+    alert('请先登录');
+    window.location.href='index.html';}
+//  document.getElementById('user_name').value = name.toString();
+  querySelector("#save").onClick.listen((_) async{
+
+    var myConfirm1 = await confirm("是否提交报名表？");
+
+    if (myConfirm1.toString()=='true'){
+      user_save();
+      alert('\n保存成功！');
+    }
+    else {alert('\n请继续填写！');}
+  });
+  querySelector("#reset").onClick.listen((_) async{
+    var myConfirm2 = await confirm("是否重置报名表？");
+    if (myConfirm2.toString()=='true'){
+      user_reset();
+      alert('\n重置成功！');
+
+    }
+    else {alert('\n请继续填写报名表！');}
+  });;
+
+
+}
+
+void user_save(){
+  var value="";
+  var radio=document.getElementsByName("for");
+  for(var i=0;i<radio.length;i++){
+    if(radio[i].checked==true){
+      value=radio[i].value;
+      break;
+    }
+  }
+  // user_for = querySelector('#for');
+  userhobby_in = querySelector('#user_hobby');
+  userhonor_in = querySelector('#user_honor');
+  userthink_in = querySelector('#user_think');
+  userfut_in= querySelector('#user_fut');
+
+
+  userfor = value.toString();
+  userhobby = userhobby_in.value;
+  userhonor = userhonor_in.value;
+  userthink = userthink_in.value;
+  userfut =  userfut_in.value;
+
+  var name=cookie.get('name');
+
+  list.add(userfor);
+  list.add(userhobby);
+  list.add(userhonor);
+  list.add(userthink);
+  list.add(userfut);
+  list.add(name);
+
+  var path = 'http://127.0.0.1:8014/stusign';
+  var httprequest = new HttpRequest();
+  httprequest
+    ..open('POST', path)
+    ..send(JSON.encode(list));
+  list=[];
+
+}
+
+void user_reset() {
+
+  document.getElementById('user_hobby').value="";
+  document.getElementById('user_think').value="";
+  document.getElementById('user_honor').value="";
+  document.getElementById('user_fut').value="";
+
+}
