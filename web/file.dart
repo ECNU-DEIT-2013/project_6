5c217880de20f045ae345e50de099c98f72be2d6
azhangyilei <azhangyilei@163.com>
2016-01-05 16:35:04 +0800

    添加注释

--- web/file.dart
+++ web/file.dart
@@ -6,6 +6,6 @@ void main(){
   var name=cookie.get('name');
   if(name==null){
    alert('请先登录');
-   window.location.href='index.html';
+   window.location.href='index.html';//使用cookie方法判断用户有无登录，如果没有登录，则会跳转到登录页面
   }
 }
--- web/studentpage.dart
+++ web/studentpage.dart
@@ -2,18 +2,15 @@ import 'dart:html';
 import 'package:cookie/cookie.dart'as cookie;
 import 'dart:convert';
 import 'package:dialog/dialog.dart';
-
 ImageElement button_3;//存放图片
 InputElement todoInput;
 List<String> my_email=[];//存放从服务器中传过来的值
 void main(){
   var name=cookie.get('name');
-document.getElementById('test').value=name.toString();
   if(name==null){
    alert('请先登录');
    window.location.href='index.html';
   }
-
   var path = 'http://127.0.0.1:8080/studentpage';//唯一的地址URL
   var httpRequest = new HttpRequest();//访问web网站的协议（网上请求文件——ftp超文本传输协议）
   httpRequest
@@ -22,7 +19,6 @@ document.getElementById('test').value=name.toString();
     ..send(JSON.encode(name))//把用户名传给服务器，服务器根据用户名进行查找与用户名匹配的信息
     ..onLoadEnd.listen((e) => requestComplete(httpRequest));
 }
-
 requestComplete(HttpRequest request) {
   if (request.status == 200) {
     //200代表数据正确拿到
